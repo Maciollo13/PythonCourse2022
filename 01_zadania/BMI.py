7077ec3c521488902350ab3cc9267ab785830770
Maciollo13 <mgerej1@gmail.com>
2022-05-09 20:04:45 +0200

    first commit

---
+++ 01_zadania/BMI.py
@@ -0,0 +1,4 @@
+masa = input("Podaj swoja mase ")
+wzrost = input("Podaj swoj wzrost ")
+BMI = float(masa) / (float(wzrost)**2)
+print("your BMI is:", round(BMI,2))
---
+++ 01_zadania/weeks.py
@@ -0,0 +1,10 @@
+tygodnie = 7
+dni = 7
+godziny = 24
+minuty = 60
+liczba_minut = tygodnie * dni * godziny * minuty
+print(liczba_minut)
+
+czas_dzien = 2
+nowa_um = 75 // (czas_dzien * 7)
+print(nowa_um)
