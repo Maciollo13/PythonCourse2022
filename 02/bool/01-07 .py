7eb749bfc6820b306f1b9b12c5d865aa6c7b2c56
Maciollo13 <mgerej1@gmail.com>
2022-05-15 22:07:59 +0200

    Update

---
+++ 02/bool/01-07 .py
@@ -0,0 +1,9 @@
+print(23+3 == 15+12)
+print(29 // 7 == 5)
+print(27 % 8 == 3)
+a = input("insert number ")
+b = int(a) % 2 == 0
+print(f"Is this number even? {b}")
+print(43 - 13 == 11 + 12)
+print(129 // 17 == 3)
+print(247 % 5 == 2)
